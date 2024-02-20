dc0bcbfcf8657cb007014ff348198441798bf197
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-02-20 16:24:30 +0300

    fixed up the post routes page and started building out the seeds folder

---
+++ seeds/Comment.js
@@ -0,0 +1,9 @@
+const { Comment } = require('../models');
+
+const commentData = [
+    {
+       comment_text: 
+        user_id:   
+        post_id: 
+    }
+]
---
+++ seeds/post.js
@@ -0,0 +1,8 @@
+const { Post } = require('../models');
+
+const postData = [
+    {
+        title: 
+        content:   
+        user_id: 
+    }
