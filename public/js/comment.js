9b851410733618111b199911a856014662014bca
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-03-01 16:10:13 +0300

    fixed my routes and my comments js so i can succesfully add a comment without any errors

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,26 +1,30 @@
 const commentForm = document.querySelector("#commentForm");
 
 const showCommentForm = () => {
-    const commentForm = document.getElementById("commentForm");
-    commentForm.style.display = "block";
-  };
-  const commentButtonHandler = async (event) => {
-    event.preventDefault()
-    console.log("adding comment");
-   const input= document.getElementById("commentInput")
-   console.log(input.value);
-    if (event.target.hasAttribute("data-id")) {
-      const id = event.target.getAttribute("data-id");
-      const response = await fetch(`/api/comment/${id}`, {
-        method: "POST",
-      });
-      if (response.ok) {
-        showCommentForm();
-        document.location.replace("/profile");
-      } else {
-        alert("Failed to delete project");
-      }
-    }
-  };
+  const commentForm = document.getElementById("commentForm");
+  commentForm.style.display = "block";
+};
+const commentButtonHandler = async (event) => {
+  event.preventDefault();
+  console.log("adding comment");
+  const input = document.getElementById("commentInput");
+  
+const body = {
+  comment: input.value,
+  user_id: input.dataset.userId,
+  post_id: input.dataset.postId
+}
+console.log(body);
+  const response = await fetch(`/api/comments`, {
+    method: "POST",
+    body: JSON.stringify(body),
+  });
+  if (response.ok) {
+    showCommentForm();
+    document.location.replace("/profile");
+  } else {
+    alert("Failed to delete project");
+  }
+};
 
-  commentForm.addEventListener("submit", commentButtonHandler);
+commentForm.addEventListener("submit", commentButtonHandler);
